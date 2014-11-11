48df8f4016c54fe0a2c427fd68471e6d89149278
Prelang Builder <builder@prelang.com>
2014-11-11 12:46:52 +0000

    Installed relation 'has_and_belongs_to_many' on model 'Recipe'

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :diets
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :eases
+  has_and_belongs_to_many :occasions
 end
